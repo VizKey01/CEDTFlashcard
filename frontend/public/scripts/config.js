87baefeb8ac76986fc98c9d3e66a960f06c2a21b
VizKey01 <102896303+VizKey01@users.noreply.github.com>
2023-10-12 03:28:43 +0700

    Update config.js

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,4 +1,12 @@
 // frontend port: 3221
 // backend port: 3222
 
-export const FRONTEND_URL = "http://3.88.222.117:3222";
+export const FRONTEND_URL = "http://3.88.62.245:3222";
+
+//responsive parts
+
+// window.addEventListener("resize", function() {
+//     if (window.innerWidth <= 800) {
+//         window.alert("screen smaller than 800");
+//     }
+//  })
